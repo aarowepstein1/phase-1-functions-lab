b543cbce5a0cde6de3b4c0aa02151e8200ab43dd
Aarow Epstein <aarowepstein1@gmail.com>
2022-12-17 15:32:13 -0700

    commit tree 58c26eb843111aa0f3641218ce6fc2969130db0c

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solution in this file!
+const hq = 42;
+function distanceFromHqInBlocks(pickup) {
+    if (hq >= pickup) {
+        const blocks = hq - pickup;
+        return blocks;
+    }
+    else {
+        const blocks = pickup - hq;
+        return blocks;
+    }
+}
+
+function distanceFromHqInFeet(pickup) {
+    return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    if (destination >= start) {
+        const blocks = destination - start;
+        const distanceInFeet = blocks * 264;
+        return distanceInFeet;
+    }
+    else {
+        const blocks = start - destination;
+        const distanceInFeet = blocks * 264;
+        return distanceInFeet;
+    }
+}
+
+function calculatesFarePrice (start, destination) {
+    const feetPrice = distanceTravelledInFeet(start,destination);
+    if (feetPrice <= 400) {
+        const fare = 0;
+        return fare;
+    } else if (feetPrice <= 2000) {
+        const fare = (feetPrice - 400) * .02;
+        return fare;
+    } else if (feetPrice <= 2500) {
+        //const fare = 25;
+        return 25;
+    } else {
+        return "cannot travel that far"
+    }
+}
